2f5e7874b3492adeeabf03c23bf0612efb959b33
juliabandeira <juveigas@gmail.com>
2023-05-11 17:31:38 +0100

    first

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,28 @@
+class JobsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    @jobs = Job.all
+    render 'jobs/index' 
+  end
+
+  def create
+    @job = Job.new(task_params)
+    if @job.save
+      render 'jobs/create'
+    end
+  end
+
+  def show
+    @job = Job.find(params[:id])
+    if @job
+      render 'jobs/show'
+    end
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:url, :employer_name, :employer_description, :job_title, :job_description, :year_of_experience, :education_requirement, :industry, :base_salary, :employment_type_id)
+  end
+end
